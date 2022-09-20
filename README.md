a92acdeee1a556da123fc6b351368ba049883e4d
Darrel Ang <darrelaiscoding@gmail.com>
2022-09-20 17:40:01 +0800

    Basic server setup

---
+++ README.md
@@ -0,0 +1,19 @@
+## About The Project
+
+- [Golang / Go Gin Framework Crash Course](https://www.youtube.com/watch?v=qR0WnWL2o1Q&list=PL3eAkoh7fypr8zrkiygiY1e9osoqjoV9w)
+- [Pragmatic Reviews](https://gitlab.com/pragmaticreviews)
+- [Original Repo: golang-gin-poc](https://gitlab.com/pragmaticreviews/golang-gin-poc)
+
+&nbsp;
+
+---
+
+&nbsp;
+
+## Notes
+
+&nbsp;
+
+---
+
+&nbsp;
---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/DarrelASandbox/go-golang-gin-poc/controller"
+	"github.com/DarrelASandbox/go-golang-gin-poc/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/posts", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":4000")
+}
