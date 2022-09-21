ce6cb7365c5932c1d48953dc0d7ac8d18b35b8b3
Darrel Ang <darrelaiscoding@gmail.com>
2022-09-21 13:16:18 +0800

    data binding & validation

--- server.go
+++ server.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/DarrelASandbox/go-golang-gin-poc/controller"
 	"github.com/DarrelASandbox/go-golang-gin-poc/middlewares"
 	"github.com/DarrelASandbox/go-golang-gin-poc/service"
 	"github.com/gin-gonic/gin"
-	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -25,15 +25,21 @@ func main() {
 	setupLogOutput()
 
 	server := gin.New()
-	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+	// server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
 	// server.Use(gin.Recovery(), gin.Logger()) is same as server := gin.Default()
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
 
-	server.POST("/posts", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
+	server.POST("/videos", func(ctx *gin.Context) {
+		err := videoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!!"})
+		}
 	})
 
 	server.Run(":4000")
